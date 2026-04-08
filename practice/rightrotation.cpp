8f2963a9b3735a1bd718eaaaa1c0470e30d00331
rahul-sonawane42 <rahulsonawane280305@gmail.com>
2026-04-08 15:24:58 +0530

    zero gravity

--- practice/rightrotation.cpp
+++ practice/rightrotation.cpp
@@ -6,7 +6,7 @@ void rotateright(int arr[],int n, int k){
     int count = 0;
     while (count < k){
         int temp = arr[n-1];
-        for (int i = n; i>0; i--){
+        for (int i = n-1; i>0; i--){
             arr[i] = arr[i-1];
         }
         arr[0] = temp;
---
+++ practice/zerogravity.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void zerograv(int arr[], int n){
+    for (int i = 0; i< n; i++){
+        if (arr[i] == 0){
+            int temp = arr[i];
+            for (int j = i; j< n; j++){
+                arr[j] = arr[j+1];
+            }
+            arr[n-1] = temp;
+        }
+    }
+}
+
+int main() {
+    int arr[] = {0,1,0,13,6,0,23,7,21,0,34,0,12,0};
+    int size = sizeof(arr)/sizeof(int);
+    zerograv(arr,size);
+
+    for (int i = 0; i < size; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
