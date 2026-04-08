0f9f6c6eca8ee7165b00e84f516ebd7baf2d70df
rahul-sonawane42 <rahulsonawane280305@gmail.com>
2026-04-08 15:06:45 +0530

    rightward rotation

---
+++ practice/rightrotation.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void rotateright(int arr[],int n, int k){
+    int count = 0;
+    while (count < k){
+        int temp = arr[n-1];
+        for (int i = n; i>0; i--){
+            arr[i] = arr[i-1];
+        }
+        arr[0] = temp;
+        count++;
+    }
+}
+
+int main() {
+    int arr[] = {1,2,3,4,5};
+    int size = sizeof(arr)/sizeof(int);
+    int k = 2;
+    rotateright(arr, size, k);
+
+    for (int i = 0; i < size; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    
+    return 0;
+}
